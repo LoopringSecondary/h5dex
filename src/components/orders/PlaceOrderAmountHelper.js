4442cb0e8b77aedfe844503c77dbc8878398b45b
houzongbao <houzongbao@126.com>
2018-06-05 15:10:10 +0800

    update placeOrder price & amount helper

--- src/components/orders/PlaceOrderAmountHelper.js
+++ src/components/orders/PlaceOrderAmountHelper.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Input,Icon } from 'antd';
-import { Modal,List,Button,Tabs,Badge } from 'antd-mobile';
+import { Modal,List,Button,Tabs,Badge,Slider } from 'antd-mobile';
 import {toBig, toHex, clearHexPrefix} from 'LoopringJS/common/formatter'
 import config from 'common/config'
 import intl from 'react-intl-universal';
@@ -29,78 +29,59 @@ function PlaceOrderAmountHelper(props) {
       >
         <div className="">
           <div className="row pt15 pb15 ml0 mr0 zb-b-b">
-            <div className="col color-black-1 text-left">
+            <div className="col color-black-1 text-left pl15">
               100%
             </div>
-            <div className="col-auto color-black-3">
+            <div className="col-auto color-black-3 pr15">
               1500.00 LRC
             </div>
           </div>
           <div className="row pt15 pb15 ml0 mr0 zb-b-b">
-            <div className="col color-black-1 text-left">
+            <div className="col color-black-1 text-left pl15">
               75%
             </div>
-            <div className="col-auto color-black-3">
+            <div className="col-auto color-black-3 pr15">
               1125.00 LRC
             </div>
           </div>
           <div className="row pt15 pb15 ml0 mr0 zb-b-b">
-            <div className="col color-black-1 text-left">
+            <div className="col color-black-1 text-left pl15">
               50%
             </div>
-            <div className="col-auto color-black-3">
+            <div className="col-auto color-black-3 pr15">
               750.00 LRC
             </div>
           </div>
           <div className="row pt15 pb15 ml0 mr0 zb-b-b">
-            <div className="col color-black-1 text-left">
+            <div className="col color-black-1 text-left pl15">
               25%
             </div>
-            <div className="col-auto color-black-3">
+            <div className="col-auto color-black-3 pr15">
               375.00 LRC
             </div>
           </div>
-          <div className="row pt15 pb15 ml0 mr0 zb-b-b">
-            <div className="col color-black-1 text-left">
+          <div className="row pt15 pb15 ml0 mr0">
+            <div className="col color-black-1 text-left pl15">
               自定义
+              <span className="ml5">10%</span>
             </div>
-            <div className="col-auto color-black-3">
+            <div className="col-auto color-black-3 pr15">
+              0.00 LRC
             </div>
+          </div>
+          <div className="mt15 pb35">
+            <Slider
+              className="ml15 mr15"
+              defaultValue={10}
+              min={0}
+              max={100}
+              onChange={()=>{}}
+              onAfterChange={()=>{}}
+            />
           </div>
         </div>
         <div className="">
-          <table className="w-100">
-            <thead>
-              <tr className="">
-                <th className="text-left bg-grey-100 pl10 pr10 pt5 pb5 font-weight-normal color-black-3">Amount</th>
-                <th className="text-right bg-grey-100 pl10 pr5 pt5 pb5 font-weight-normal color-black-3">Buy</th>
-                <th className="text-left bg-grey-100 pl5 pr10 pt5 pb5 font-weight-normal color-black-3">Sell</th>
-                <th className="text-right bg-grey-100 pl10 pr10 pt5 pb5 font-weight-normal color-black-3">Amount</th>
-              </tr>
-            </thead>
-            <tbody>
-                {
-                  [1,2,3,4,5].map((item,index)=>
-                    <tr key={index}>
-                      <td className="pl10 pr10 pt10 pb10 zb-b-b color-black-2 text-left align-middle">
-                        1000.00
-                      </td>
-                      <td className="pl10 pr5 pt10 pb10 zb-b-b text-right text-up align-middle">
-                        0.0001500
-                        <div className="fs12 color-black-3 mr5">￥8.52</div>
-                      </td>
-                      <td className="pl5 pr10 pt10 pb10 zb-b-b text-left text-down align-middle">
-                        0.0001500
-                        <div className="fs12 color-black-3 mr5">￥8.52</div>
-                      </td>
-                      <td className="pl10 pr10 pt10 pb10 zb-b-b color-black-2 text-right align-middle">
-                        1000.00
-                      </td>
-                    </tr>
-                  )
-                }
-            </tbody>
-          </table>
+          <DepthList />
         </div>
       </Tabs>
 
@@ -109,3 +90,40 @@ function PlaceOrderAmountHelper(props) {
   )
 }
 export default PlaceOrderAmountHelper
+
+export const DepthList = ()=>{
+  return (
+    <table className="w-100">
+      <thead>
+        <tr className="">
+          <th className="zb-b-b text-left pl10 pr10 pt5 pb5 font-weight-normal color-black-3 fs16">Amount</th>
+          <th className="zb-b-b text-right pl10 pr10 pt5 pb5 font-weight-normal color-black-3 fs16">Buy</th>
+          <th className="zb-b-b text-left pl10 pr10 pt5 pb5 font-weight-normal color-black-3 fs16">Sell</th>
+          <th className="zb-b-b text-right pl10 pr10 pt5 pb5 font-weight-normal color-black-3 fs16">Amount</th>
+        </tr>
+      </thead>
+      <tbody>
+          {
+            [1,2,3,4,5].map((item,index)=>
+              <tr key={index}>
+                <td className="fs16 pl10 pr10 pt10 pb10 zb-b-b color-black-2 text-left align-middle">
+                  1000.0000
+                </td>
+                <td className="fs16 pl10 pr10 pt10 pb10 zb-b-b text-right text-up align-middle">
+                  0.00015000
+                  <div className="fs12 color-black-4 mr5">￥8.52</div>
+                </td>
+                <td className="fs16 pl10 pr10 pt10 pb10 zb-b-b text-left text-down align-middle">
+                  0.00015000
+                  <div className="fs12 color-black-4 mr5">￥8.52</div>
+                </td>
+                <td className="fs16 pl10 pr10 pt10 pb10 zb-b-b color-black-2 text-right align-middle">
+                  1000.0000
+                </td>
+              </tr>
+            )
+          }
+      </tbody>
+    </table>
+  )
+}
--- src/components/orders/PlaceOrderPriceHelper.js
+++ src/components/orders/PlaceOrderPriceHelper.js
@@ -14,6 +14,8 @@ import * as fm from 'LoopringJS/common/formatter'
 import QRCode from 'qrcode.react';
 import Alert from 'LoopringUI/components/Alert'
 
+import {DepthList} from './PlaceOrderAmountHelper'
+
 function PlaceOrderPriceHelper(props) {
   const tabs = [
     { title: <Badge >Qucik</Badge> },
@@ -24,46 +26,24 @@ function PlaceOrderPriceHelper(props) {
       <div className="pt15 pb15 fs24 color-black-1 zb-b-b">Price Helper</div>
       <div className="">
         <div className="row pt15 pb15 ml0 mr0 zb-b-b align-items-center">
-          <div className="col color-black-1 text-left">
+          <div className="col color-black-1 text-left pl10">
+            Current Price
+          </div>
+          <div className="col-auto color-black-2">
+            <span className="color-black-4 mr5 fs14">￥8.96</span>0.0001650 ETH
+          </div>
+        </div>
+        <div className="row pt15 pb15 ml0 mr0 zb-b-b align-items-center">
+          <div className="col color-black-1 text-left pl10">
             Last Price
           </div>
           <div className="col-auto color-black-2">
-            <span className="color-black-4 mr5">￥8.52</span>0.0001500 ETH
+            <span className="color-black-4 mr5 fs14">￥8.52</span>0.0001500 ETH
           </div>
         </div>
+        <DepthList />
       </div>
-      <table className="w-100">
-        <thead>
-          <tr className="">
-            <th className="text-left bg-grey-100 pl10 pr10 pt5 pb5 font-weight-normal color-black-3">Amount</th>
-            <th className="text-right bg-grey-100 pl10 pr5 pt5 pb5 font-weight-normal color-black-3">Buy</th>
-            <th className="text-left bg-grey-100 pl5 pr10 pt5 pb5 font-weight-normal color-black-3">Sell</th>
-            <th className="text-right bg-grey-100 pl10 pr10 pt5 pb5 font-weight-normal color-black-3">Amount</th>
-          </tr>
-        </thead>
-        <tbody>
-            {
-              [1,2,3,4,5].map((item,index)=>
-                <tr key={index}>
-                  <td className="pl10 pr10 pt10 pb10 zb-b-b color-black-2 text-left align-middle">
-                    1000.00
-                  </td>
-                  <td className="pl10 pr5 pt10 pb10 zb-b-b text-right text-up align-middle">
-                    0.0001500
-                    <div className="fs12 color-black-4 mr5">￥8.52</div>
-                  </td>
-                  <td className="pl5 pr10 pt10 pb10 zb-b-b text-left text-down align-middle">
-                    0.0001500
-                    <div className="fs12 color-black-4 mr5">￥8.52</div>
-                  </td>
-                  <td className="pl10 pr10 pt10 pb10 zb-b-b color-black-2 text-right align-middle">
-                    1000.00
-                  </td>
-                </tr>
-              )
-            }
-        </tbody>
-      </table>
+
     </div>
   )
 }
