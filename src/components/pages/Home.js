9d07db7b917a88a1ba9f610c8d1baef26c53bcd4
houzongbao <houzongbao@126.com>
2018-06-06 23:40:03 +0800

    update Home ui

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -19,99 +19,101 @@ class Home extends React.Component {
   }
   render(){
     return (
-      <div>
-        <NavBar
-          mode="light"
-          icon={null && <Icon type="left" />}
-          onLeftClick={() => console.log('onLeftClick')}
-          rightContent={null && [
-            <Icon key="1" type="ellipsis" />,
-          ]}
-          leftContent={[
-            <WebIcon key="1" type="menu-fold" />,
-          ]}
-        >
-        Markets
-        </NavBar>
-        <div style={{ height: '100vh-44px', width: '100%', top: 0 }}>
-                <TabBar
-                  unselectedTintColor="#949494"
-                  tintColor="#33A3F4"
-                  barTintColor="white"
-                  hidden={this.state.hidden}
+      <div style={{ position:"fiexd",height: "100vh", width: '100%', top: "0",}}>
+              <TabBar
+                unselectedTintColor="#949494"
+                tintColor="#33A3F4"
+                barTintColor="white"
+                hidden={this.state.hidden}
+              >
+                <TabBar.Item
+                  title="Markets"
+                  key="markets"
+                  icon={<div style={{
+                    width: '22px',
+                    height: '22px',
+                    background: 'url(https://zos.alipayobjects.com/rmsportal/sifuoDUQdAFKAVcFGROC.svg) center center /  21px 21px no-repeat' }}
+                  />
+                  }
+                  selectedIcon={<div style={{
+                    width: '22px',
+                    height: '22px',
+                    background: 'url(https://zos.alipayobjects.com/rmsportal/iSrlOTqrKddqbOmlvUfq.svg) center center /  21px 21px no-repeat' }}
+                  />
+                  }
+                  selected={this.state.selectedTab === 'markets'}
+                  badge={null && 1}
+                  onPress={() => {
+                    this.setState({
+                      selectedTab: 'markets',
+                    });
+                  }}
+                  data-seed="logId"
                 >
-                  <TabBar.Item
-                    title="Markets"
-                    key="markets"
-                    icon={<div style={{
-                      width: '22px',
-                      height: '22px',
-                      background: 'url(https://zos.alipayobjects.com/rmsportal/sifuoDUQdAFKAVcFGROC.svg) center center /  21px 21px no-repeat' }}
-                    />
-                    }
-                    selectedIcon={<div style={{
-                      width: '22px',
-                      height: '22px',
-                      background: 'url(https://zos.alipayobjects.com/rmsportal/iSrlOTqrKddqbOmlvUfq.svg) center center /  21px 21px no-repeat' }}
-                    />
-                    }
-                    selected={this.state.selectedTab === 'markets'}
-                    badge={null && 1}
-                    onPress={() => {
-                      this.setState({
-                        selectedTab: 'markets',
-                      });
-                    }}
-                    data-seed="logId"
+                <div className="position-relative"  style={{paddingTop:'45px'}}>
+                  <NavBar
+                    style={{top:'0px',position:'fixed',zIndex:10}}
+                    className="w-100"
+                    mode="light"
+                    icon={null && <Icon type="left" />}
+                    onLeftClick={() => console.log('onLeftClick')}
+                    rightContent={null && [
+                      <Icon key="1" type="ellipsis" />,
+                    ]}
+                    leftContent={[
+                      <WebIcon key="1" type="menu-fold" />,
+                    ]}
                   >
-                    <Tickers.ListTickers />
-                  </TabBar.Item>
-                  <TabBar.Item
-                    icon={
-                      <div style={{
-                        width: '22px',
-                        height: '22px',
-                        background: 'url(https://gw.alipayobjects.com/zos/rmsportal/BTSsmHkPsQSPTktcXyTV.svg) center center /  21px 21px no-repeat' }}
-                      />
-                    }
-                    selectedIcon={
-                      <div style={{
-                        width: '22px',
-                        height: '22px',
-                        background: 'url(https://gw.alipayobjects.com/zos/rmsportal/ekLecvKBnRazVLXbWOnE.svg) center center /  21px 21px no-repeat' }}
-                      />
-                    }
-                    title="Trade"
-                    key="trade"
-                    badge={null && 'new'}
-                    selected={this.state.selectedTab === 'trade'}
-                    onPress={() => {
-                      this.setState({
-                        selectedTab: 'trade',
-                      });
-                    }}
-                    data-seed="logId1"
-                  >
-                    <Orders.PlaceOrderFormStand />
-                  </TabBar.Item>
-                  <TabBar.Item
-                    icon={{ uri: 'https://zos.alipayobjects.com/rmsportal/asJMfBrNqpMMlVpeInPQ.svg' }}
-                    selectedIcon={{ uri: 'https://zos.alipayobjects.com/rmsportal/gjpzzcrPMkhfEqgbYvmN.svg' }}
-                    title="My"
-                    key="my"
-                    selected={this.state.selectedTab === 'my'}
-                    onPress={() => {
-                      this.setState({
-                        selectedTab: 'my',
-                      });
-                    }}
-                  >
-                    333
-                  </TabBar.Item>
-                </TabBar>
-              </div>
-      </div>
+                  Markets
+                  </NavBar>
+                  <Tickers.ListTickers />
+                </div>
 
+                </TabBar.Item>
+                <TabBar.Item
+                  icon={
+                    <div style={{
+                      width: '22px',
+                      height: '22px',
+                      background: 'url(https://gw.alipayobjects.com/zos/rmsportal/BTSsmHkPsQSPTktcXyTV.svg) center center /  21px 21px no-repeat' }}
+                    />
+                  }
+                  selectedIcon={
+                    <div style={{
+                      width: '22px',
+                      height: '22px',
+                      background: 'url(https://gw.alipayobjects.com/zos/rmsportal/ekLecvKBnRazVLXbWOnE.svg) center center /  21px 21px no-repeat' }}
+                    />
+                  }
+                  title="Trade"
+                  key="trade"
+                  badge={null && 'new'}
+                  selected={this.state.selectedTab === 'trade'}
+                  onPress={() => {
+                    this.setState({
+                      selectedTab: 'trade',
+                    });
+                  }}
+                  data-seed="logId1"
+                >
+                  <Orders.PlaceOrderFormStand />
+                </TabBar.Item>
+                <TabBar.Item
+                  icon={{ uri: 'https://zos.alipayobjects.com/rmsportal/asJMfBrNqpMMlVpeInPQ.svg' }}
+                  selectedIcon={{ uri: 'https://zos.alipayobjects.com/rmsportal/gjpzzcrPMkhfEqgbYvmN.svg' }}
+                  title="My"
+                  key="my"
+                  selected={this.state.selectedTab === 'my'}
+                  onPress={() => {
+                    this.setState({
+                      selectedTab: 'my',
+                    });
+                  }}
+                >
+                  333
+                </TabBar.Item>
+              </TabBar>
+      </div>
     )
   }
 }
