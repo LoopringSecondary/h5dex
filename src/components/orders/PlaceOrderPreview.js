56241bdc5f6c2db905b2d868e14c171ab5c0ad6f
houzongbao <houzongbao@126.com>
2018-06-07 18:00:38 +0800

    update placeOrder preview

--- src/components/orders/PlaceOrderPreview.js
+++ src/components/orders/PlaceOrderPreview.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Input,Icon } from 'antd';
-import { Modal,List,Button } from 'antd-mobile';
+import { Modal,List,Button,Accordion } from 'antd-mobile';
 import {toBig, toHex, clearHexPrefix} from 'LoopringJS/common/formatter'
 import config from 'common/config'
 import intl from 'react-intl-universal';
@@ -14,32 +14,130 @@ import * as fm from 'LoopringJS/common/formatter'
 import QRCode from 'qrcode.react';
 import Alert from 'LoopringUI/components/Alert'
 
+const OrderMetaItem = (props) => {
+  const {label, value} = props
+  return (
+    <div className="row ml0 mr0 pt5 pb5 pl0 pr0">
+      <div className="col">
+        <div className="fs16 color-black-2 lh25 text-left">{label}</div>
+      </div>
+      <div className="col-auto text-right">
+        <div className="fs16 color-black-1 text-wrap lh25">{value}</div>
+      </div>
+    </div>
+  )
+}
+const WalletItem = (props) => {
+  const {title, description,icon,layout,showArrow} = props
+  if(layout === 'vertical'){
+    return (
+      <div className="pl10 pr10 pt15 pb15">
+        <div className="text-center text-primary">
+          <i className={`fs28 icon-${icon}`}></i>
+        </div>
+        <div className="fs18">{title}</div>
+      </div>
+    )
+  }else{
+    return (
+      <div className="row pt10 pb10 pl0 pr0 align-items-center zb-b-b">
+        <div className="col-auto pr5 text-center text-primary" style={{minWidth:'40px'}}>
+          <i className={`fs24 icon-${icon}`}></i>
+        </div>
+        <div className="col pl10">
+          <div className="fs18 color-black-1 text-wrap text-left">{title}</div>
+          { description && <div className="fs16 color-black-2">{description}</div> }
+        </div>
+        {showArrow &&
+          <div className="col-auto text-right">
+            <Icon type="right" />
+          </div>
+        }
+      </div>
+     )
+  }
+}
+
+const PlaceOrderResult = ({
+  }) => {
+
+  return (
+    <div className="">
+        {
+          true &&
+          <div className="text-center p35">
+            <i className={`fs50 icon-success`}></i>
+            <div className="fs18 color-black-1">订单提交成功！</div>
+            <div className="mt10">
+              <Button className="m5" type="default"> 查看订单 </Button>
+              <Button className="m5" type="default"> 继续下单 </Button>
+            </div>
+          </div>
+        }
+        {
+          true &&
+          <div className="text-center p35">
+            <Icon type="close-circle" className="fs50 text-error" />
+            <div className="fs18 color-black-1 mt15">您取消了订单提交</div>
+            <div className="mt10">
+              <Button className="m5" type="default"> 返回上级 </Button>
+              <Button className="m5" type="default"> 返回交易页 </Button>
+            </div>
+          </div>
+        }
+    </div>
+  );
+};
 function PlaceOrderPreview(props) {
   const {placeOrderPreview} = props
   const {side} = placeOrderPreview
   return (
     <div className="">
-        <List renderHeader={() => <div className="pt15 pb15 color-black-1 fs22">{intl.get(`common.${side}`)} LRC</div>} className="popup-list">
-          <List.Item extra="10000 LRC">{intl.get('common.sell')}</List.Item>
-          <List.Item extra="10 WETH">{intl.get('common.buy')}</List.Item>
-          <List.Item extra="0.00100">{intl.get('common.price')}</List.Item>
-          {false && <List.Item extra={intl.get('order_type.market_order')}>{intl.get('place_order.order_type')}</List.Item> }
-          <List.Item extra="10LRC">{intl.get('common.lrc_fee')}</List.Item>
-          { false && <List.Item extra="30%">{intl.get('common.margin_split')}</List.Item> }
-          { false && <List.Item extra="2018-06-10 10:00">{intl.get('place_order.order_since')}</List.Item> }
-          <List.Item extra="06-11 10:00">{intl.get('place_order.order_until')}</List.Item>
-          <List.Item>
-            {
-              side === 'buy' &&
-              <Button type="" className="bg-green-500 color-white" onClick={()=>{}}>Place Order Now</Button>
-            }
-            {
-              side === 'sell' &&
-              <Button className="bg-red-500 color-white" onClick={()=>{}}>Place Order Now</Button>
-            }
-            <Button className="mt10" default="ghost" onClick={()=>{}}>No,Thanks</Button>
-          </List.Item>
-        </List>
+        {false && <div className="pt15 pb15 color-black-1 fs22">{intl.get(`common.${side}`)} LRC</div>}
+        <Accordion defaultActiveKey="0" className="" onChange={()=>{}}>
+          <Accordion.Panel header={<div className="text-left">1. 确认订单信息</div>}>
+            <div className="pt10 pb10 bg-grey-100">
+              <OrderMetaItem label="买入" value="10000 LRC" />
+              <OrderMetaItem label="卖出" value="25 ETH" />
+              <OrderMetaItem label="价格" value="0.00025 ETH" />
+              <OrderMetaItem label="矿工撮合费" value="2.2 LRC" />
+              <OrderMetaItem label="订单有效期" value="06-10 10:38 ~ 06-30 10:38" />
+            </div>
+          </Accordion.Panel>
+          <Accordion.Panel header={<div className="text-left">2. 选择支付方式</div>} className="pad">
+            <div className="row ml0 mr0 bg-grey-100">
+              <div className="col-4 zb-b-r">
+                <WalletItem icon="metamaskwallet" title="Loopr" layout="vertical" />
+              </div>
+              <div className="col-4 zb-b-r">
+                <WalletItem icon="ledgerwallet" title="imToken" layout="vertical" />
+              </div>
+              <div className="col-4">
+                <WalletItem icon="trezorwallet" title="MyToken" layout="vertical" />
+              </div>
+            </div>
+          </Accordion.Panel>
+          <Accordion.Panel header={<div className="text-left">3. 等待支付结果</div>} className="pad">
+
+          </Accordion.Panel>
+
+        </Accordion>
+        {
+          false &&
+          <List className="popup-list">
+            <List.Item>
+              {
+                side === 'buy' &&
+                <Button type="" className="bg-green-500 color-white" onClick={()=>{}}>Place Order Now</Button>
+              }
+              {
+                side === 'sell' &&
+                <Button className="bg-red-500 color-white" onClick={()=>{}}>Place Order Now</Button>
+              }
+              <Button className="mt10" default="ghost" onClick={()=>{}}>No,Thanks</Button>
+            </List.Item>
+          </List>
+        }
 
     </div>
   )
