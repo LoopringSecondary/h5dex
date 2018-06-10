107d8488dde7956c3886d9d06c05f8df6956cce3
houzongbao <houzongbao@126.com>
2018-06-10 18:39:14 +0800

    update palce Order steps

--- src/components/orders/PlaceOrderPreview.js
+++ src/components/orders/PlaceOrderPreview.js
@@ -13,6 +13,8 @@ import * as uiFormatter from 'modules/formatter/common'
 import * as fm from 'LoopringJS/common/formatter'
 import QRCode from 'qrcode.react';
 import Alert from 'LoopringUI/components/Alert'
+import {Pages,Page} from 'LoopringUI/components/Pages'
+import {connect} from 'dva'
 
 const OrderMetaItem = (props) => {
   const {label, value} = props
@@ -40,16 +42,19 @@ const WalletItem = (props) => {
     )
   }else{
     return (
-      <div className="row pt10 pb10 pl0 pr0 align-items-center zb-b-b">
-        <div className="col-auto pr5 text-center text-primary" style={{minWidth:'40px'}}>
-          <i className={`fs24 icon-${icon}`}></i>
+      <div className="row p15 ml0 mr0 align-items-center zb-b-b no-gutters">
+        <div className="col-auto pr5 text-center color-black-1 fs24 " style={{minWidth:'40px'}}>
+          {typeof icon === 'string' &&
+            <i className={`icon-${icon}`}></i>
+          }
+          {typeof icon !== 'string' && icon}
         </div>
         <div className="col pl10">
-          <div className="fs18 color-black-1 text-wrap text-left">{title}</div>
+          <div className="fs20 color-black-1 text-wrap text-left">{title}</div>
           { description && <div className="fs16 color-black-2">{description}</div> }
         </div>
         {showArrow &&
-          <div className="col-auto text-right">
+          <div className="col-auto text-right color-black-3">
             <Icon type="right" />
           </div>
         }
@@ -93,53 +98,88 @@ const PlaceOrderResult = ({
 function PlaceOrderPreview(props) {
   const {placeOrderPreview} = props
   const {side} = placeOrderPreview
-  const steps = [{
-    title: 'Finished',
-  }, {
-    title: 'Progress',
-  }, {
-    title: 'Waiting',
-  }].map((s, i) => <Steps.Step key={i} title={s.title} description={s.description} />);
+
   return (
     <div className="">
-        <div className="p15 color-black-1 fs22 zb-b-b text-center">Place Order</div>
-        <div hidden className="p15 text-left">
-          <Steps direction="horizontal">
-             <Steps.Step title={<div className="fs18">Order</div>}  icon={<Icon type="check-circle-o" />} />
-             <Steps.Step title={<div className="fs18">Wallet</div>} icon={<Icon type="check-circle-o" />} />
-             <Steps.Step title={<div className="fs18">Result</div>}  icon={<Icon type="check-circle-o" />} />
-          </Steps>
-        </div>
-        <div className="p20 bg-white">
-          <div className="pb20 row ml0 mr0 no-gutters align-items-center justify-content-center">
-            <div className="col-auto">
-              <div className=" color-black-1 text-center" style={{width:"40px",height:'40px',lineHeight:'38px',borderRadius:'50em',border:"1px solid #000"}}>
-                <i className={`icon-LRC fs28`}/>
+        <Pages active="order">
+          <Page id="order" render={({page})=>
+            <div>
+              <div className="p15 color-black-1 fs22 zb-b-b text-center">
+                <div className="row">
+                  <div className="col text-left">
+                    <Icon type="close"/>
+                  </div>
+                  <div className="col-auto">Place Order</div>
+                  <div className="col"></div>
+                </div>
+              </div>
+              <div className="p20 bg-white">
+                <div className="pb20 row ml0 mr0 no-gutters align-items-center justify-content-center">
+                  <div className="col-auto">
+                    <div className=" color-black-1 text-center" style={{width:"40px",height:'40px',lineHeight:'38px',borderRadius:'50em',border:"1px solid #000"}}>
+                      <i className={`icon-LRC fs28`}/>
+                    </div>
+                  </div>
+                  <div className="col-auto pl25 pr25 text-center">
+                    <Icon type="swap" className={`color-black-1 fs20`} />
+                  </div>
+                  <div className="col-auto">
+                    <div className="color-black-1 text-center" style={{width:"40px",height:'40px',lineHeight:'38px',borderRadius:'50em',border:"1px solid #000"}}>
+                      <i className={`icon-WETH fs28`}/>
+                    </div>
+                  </div>
+                </div>
+                <OrderMetaItem label="买入" value="10000 LRC" />
+                <OrderMetaItem label="卖出" value="25 ETH" />
+                <OrderMetaItem label="价格" value="0.00025 ETH" />
+                <OrderMetaItem label="矿工撮合费" value="2.2 LRC" />
+                <OrderMetaItem label="订单有效期" value="06-10 10:38 ~ 06-30 10:38" />
+                {
+                  side === 'buy' &&
+                  <Button type="" className="bg-grey-900 color-white mt15" onClick={page.gotoPage.bind(this,{id:'wallet'})}>Next</Button>
+                }
+                {
+                  side === 'sell' &&
+                  <Button className="bg-grey-900 color-white mt15" onClick={page.gotoPage.bind(this,{id:'wallet'})}>Next</Button>
+                }
               </div>
             </div>
-            <div className="col-auto pl25 pr25 text-center">
-              <Icon type="swap" className={`color-black-1 fs20`} />
-            </div>
-            <div className="col-auto">
-              <div className="color-black-1 text-center" style={{width:"40px",height:'40px',lineHeight:'38px',borderRadius:'50em',border:"1px solid #000"}}>
-                <i className={`icon-WETH fs28`}/>
+          }/>
+          <Page id="wallet" render={({page})=>
+            <div className="div">
+              <div className="p15 color-black-1 fs22 zb-b-b text-center">
+                <div className="row">
+                  <div className="col text-left">
+                    <Icon type="left" onClick={page.gotoPage.bind(this,{id:'order'})}/>
+                  </div>
+                  <div className="col-auto">Select Wallet</div>
+                  <div className="col"></div>
+                </div>
+              </div>
+              <div className="bg-white">
+                <WalletItem icon="metamaskwallet" title="Loopr" showArrow={true}/>
+                <WalletItem icon="ledgerwallet" title="imToken" showArrow={true}/>
+                <WalletItem icon="trezorwallet" title="MyToken" showArrow={true}/>
+                <WalletItem icon={<Icon type="hourglass" />} title="More Wallet" showArrow={true}/>
               </div>
             </div>
-          </div>
-          <OrderMetaItem label="买入" value="10000 LRC" />
-          <OrderMetaItem label="卖出" value="25 ETH" />
-          <OrderMetaItem label="价格" value="0.00025 ETH" />
-          <OrderMetaItem label="矿工撮合费" value="2.2 LRC" />
-          <OrderMetaItem label="订单有效期" value="06-10 10:38 ~ 06-30 10:38" />
-          {
-            side === 'buy' &&
-            <Button type="" className="bg-grey-900 color-white mt15" onClick={()=>{}}>Next</Button>
-          }
-          {
-            side === 'sell' &&
-            <Button className="bg-grey-900 color-white mt15" onClick={()=>{}}>Next</Button>
-          }
-        </div>
+          }/>
+          <Page id="result" render={({page})=>
+            <div className="div">
+              <div className="p15 color-black-1 fs22 zb-b-b text-center">
+                <div className="row">
+                  <div className="col text-left">
+                    <Icon type="close"/>
+                  </div>
+                  <div className="col-auto">Result</div>
+                  <div className="col"></div>
+                </div>
+              </div>
+              <PlaceOrderResult />
+            </div>
+          }/>
+        </Pages>
+
         {false && <Accordion accordion={true} defaultActiveKey="0" className="" onChange={()=>{}}>
           <Accordion.Panel header={<div className="text-left">1. 确认订单信息</div>}>
 
@@ -183,4 +223,4 @@ function PlaceOrderPreview(props) {
     </div>
   )
 }
-export default PlaceOrderPreview
+export default connect()(PlaceOrderPreview)
