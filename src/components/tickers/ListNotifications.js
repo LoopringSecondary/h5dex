c471510c723fd1aefbc4867ac5a48ab7b5f39606
houzongbao <houzongbao@126.com>
2018-06-12 21:50:29 +0800

    update todos

--- src/components/tickers/ListNotifications.js
+++ src/components/tickers/ListNotifications.js
@@ -4,54 +4,104 @@ import {TickersFm,TickerFm} from 'modules/tickers/formatters'
 import storage from '../../modules/storage'
 import intl from 'react-intl-universal'
 import routeActions from 'common/utils/routeActions'
-import { ListView,Button,Tabs } from 'antd-mobile'
-import { Switch } from 'antd'
+import { ListView,Button,Tabs,NavBar,Icon,SegmentedControl  } from 'antd-mobile'
+import { Switch,Icon as WebIcon} from 'antd'
 
-const TodoItem = ({item,actions,key,index})=>{
+const TodoItem = ({item={},actions,key,index})=>{
     // if(!item){ return null }
     // const tickerFm = new TickerFm(item)
+    console.log('todo item',item)
     const gotoDetail = ()=>{
       routeActions.gotoPath('/trade/detail')
     }
     return (
-      <div className="row ml0 mr0 p10 align-items-center zb-b-b no-gutters" onClick={gotoDetail}>
-        <div className="col-auo text-left">
-          {
-            index%2 === 0 &&
-            <i className="loopring-icon loopring-icon-approve fs24"></i>
-          }
-          {
-            index%2 === 1 &&
-            <i className="loopring-icon loopring-icon-approve fs24"></i>
-          }
+      <div>
+        <div className="row ml0 mr0 p15 align-items-center zb-b-b no-gutters" onClick={()=>{}}>
+          <div className="col-auo pr15 color-black text-center">
+              <i className={`icon-${item.symbol} fs24 d-block`} style={{width:'32px',height:'32px',lineHeight:'32px',border:'1px solid #000',borderRadius:'50em'}}></i>
+          </div>
+          <div className="col text-left">
+            <div>
+              <div className="fs20 color-black-1">
+                {
+                  item.type === 'allowance' && `${item.symbol} is disabled for orders`
+                }
+                {
+                  item.type === 'balance' && `${item.symbol} balance is insufficient`
+                }
+              </div>
+              {
+                item.type === 'balance' &&
+                <div className="fs18 color-black-3">
+                      <div className="lh25">
+                        <span className="d-inline-block" style={{width:'100px'}}>Balance</span>
+                        1000.00 {item.symbol}
+                      </div>
+                      <div className="lh25">
+                        <span className="d-inline-block" style={{width:'100px'}}>Selling</span>
+                        5000.00 {item.symbol}
+                      </div>
+                      <div className="lh25">
+                        <span className="d-inline-block" style={{width:'100px'}}>Lack</span>
+                        4000.00 {item.symbol}
+                      </div>
+                      <Button inline={true} type="primary" size="small" className="mr5 mt5" href="">Receive</Button>
+                      <Button inline={true} type="primary" size="small" className="mr5 mt5" href="">Buy</Button>
+                      <Button inline={true} type="ghost" size="small" className="mr5 mt5" href="">View Orders</Button>
+                </div>
+              }
+            </div>
+          </div>
+          <div className="col-auto">
+            {
+              item.type === 'allowance' &&
+              <div>
+                <Switch size="" defaultChecked={false} />
+              </div>
+            }
+            {
+              false && item.type === 'balance' &&
+              <div>
+                <a className="">Detail</a>
+              </div>
+            }
+
+          </div>
         </div>
-        <div className="col text-left">
-          <div className="fs20 color-black-1">Enable LRC to Trade</div>
-          <div className="fs16 color-black-3">Once enable, valid for all orders</div>
-        </div>
-        <div className="col-auto text-left">
-          <Switch size="small" defaultChecked />
-        </div>
+
       </div>
+
     )
 }
 
-
 const data = [
   {
-    img: 'https://zos.alipayobjects.com/rmsportal/dKbkpPXKfvZzWCM.png',
-    title: 'Meet hotel',
-    des: '不是所有的兼职汪都需要风吹日晒',
+    symbol: 'EOS',
+    title: 'EOS balance is insufficient for orders',
+    type:'balance',
+
   },
   {
-    img: 'https://zos.alipayobjects.com/rmsportal/XmwCzSeJiqpkuMB.png',
-    title: 'McDonald\'s invites you',
-    des: '不是所有的兼职汪都需要风吹日晒',
+    symbol: 'WETH',
+    title: 'WETH balance is insufficient for orders',
+    type:'balance',
   },
   {
-    img: 'https://zos.alipayobjects.com/rmsportal/hfVtzEhPzTUewPm.png',
-    title: 'Eat the week',
-    des: '不是所有的兼职汪都需要风吹日晒',
+    symbol: 'LRC',
+    title: 'LRC balance is insufficient for orders',
+    type:'balance',
+  },
+  {
+    symbol: 'EOS',
+    type:"allowance",
+  },
+  {
+    symbol: 'WETH',
+    type:"allowance",
+  },
+  {
+    symbol: 'LRC',
+    type:"allowance",
   },
 ];
 const NUM_ROWS = 15;
@@ -86,7 +136,8 @@ class ListTickers extends React.Component {
 
     // simulate initial Ajax
     setTimeout(() => {
-      this.rData = genData();
+      // this.rData = genData();
+      this.rData = data;
       this.setState({
         dataSource: this.state.dataSource.cloneWithRows(this.rData),
         isLoading: false,
@@ -116,26 +167,27 @@ class ListTickers extends React.Component {
           index = data.length - 1;
         }
         const obj = data[index--];
+        console.log('obj',obj)
         return (
-          <TodoItem key={rowID} index={rowID} />
+          <TodoItem key={rowID} index={rowID} item={obj} />
         );
       };
       return (
-          <Tabs
-            tabs={
-              [
-                { title: <div className="fs20">Todos</div> },
-                { title: <div className="fs20">Messages</div> },
-              ]
-            }
-            tabBarBackgroundColor={"#fff"}
-            tabBarActiveTextColor={"#000"}
-            tabBarInactiveTextColor={"rgba(0,0,0,0.3)"}
-            tabBarTextStyle={{}}
-            initialPage={1}
-            onChange={(tab, index) => {}}
-            onTabClick={(tab, index) => { }}
-          >
+          <div className="tabs-no-border no-underline"  style={{height:'100%'}}>
+            <NavBar
+              className="w-100 zb-b-b"
+              mode="light"
+              icon={null && <Icon type="left" />}
+              onLeftClick={() => console.log('onLeftClick')}
+              leftContent={ [
+                <WebIcon key="1" type="left" className="color-black-1" />,
+              ]}
+              rightContent={null && [
+                <WebIcon key="1" type="search" className="color-black-1" />,
+              ]}
+            >
+              <SegmentedControl values={['Todos', 'Messages']} style={{width:'180px',height:'32px'}}/>
+            </NavBar>
             <ListView
               ref={el => this.lv = el}
               dataSource={this.state.dataSource}
@@ -144,7 +196,7 @@ class ListTickers extends React.Component {
               renderRow={row}
               className="am-list"
               pageSize={5}
-              useBodyScroll={false}
+              useBodyScroll={true}
               style={{
                  height: "100%",
                  overflow: 'auto',
@@ -154,10 +206,32 @@ class ListTickers extends React.Component {
               onEndReached={this.onEndReached}
               onEndReachedThreshold={10}
             />
-            <div className="p50">
-              Messages ToDO
-            </div>
-          </Tabs>
+            {
+              false &&
+              <Tabs
+                tabs={
+                  [
+                    { title: <div className="fs20">Todos</div> },
+                    { title: <div className="fs20">Messages</div> },
+                  ]
+                }
+                swipeable={false}
+                tabBarBackgroundColor={"#fff"}
+                tabBarActiveTextColor={"#000"}
+                tabBarInactiveTextColor={"rgba(0,0,0,0.3)"}
+                tabBarTextStyle={{}}
+                initialPage={0}
+                onChange={(tab, index) => {}}
+                onTabClick={(tab, index) => { }}
+              >
+
+                <div className="p50">
+                  Messages TODO
+                </div>
+              </Tabs>
+            }
+          </div>
+
       )
   }
 }
