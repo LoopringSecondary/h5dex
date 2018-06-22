cd3eeb108ae80446d2eae13d5de7de748e9667eb
houzongbao <houzongbao@126.com>
2018-06-22 15:28:36 +0800

    update cancel order

--- src/dex/orders/HelperOfMyMarketOrders.js
+++ src/dex/orders/HelperOfMyMarketOrders.js
@@ -20,7 +20,6 @@ const HelperOfMyOrders = ({orders={},dispatch})=>{
       })
   }
   const cancelOrder= (item,e)=>{
-    console.log('arguments',arguments)
     e.stopPropagation()
     Modal.alert("确认取消当前订单？", "Buy 100.00 LRC ",[
       { text: 'No', onPress: () => console.log('cancel'), style: 'default' },
@@ -28,9 +27,8 @@ const HelperOfMyOrders = ({orders={},dispatch})=>{
     ])
   }
   const cancelOrderByTokenPair = (e)=>{
-    console.log('arguments',arguments)
     e.stopPropagation()
-    Modal.alert("取消全部 LRC-WETH 订单？", "5 LRC-WETH open orders",[
+    Modal.alert("取消全部 LRC-WETH 订单？", "5 open orders of LRC-WETH",[
       { text: 'No', onPress: () => console.log('cancel'), style: 'default' },
       { text: 'Yes', onPress: () => console.log('ok') },
     ])
@@ -43,7 +41,7 @@ const HelperOfMyOrders = ({orders={},dispatch})=>{
           <tr>
             <th className="text-left pt10 pb10 pl5 pr5 font-weight-normal color-black-3 zb-b-b">
               Price
-              <span className="color-black-4 ml5 fs10">{tokens.right}</span>
+              <span hidden className="color-black-4 ml5 fs10">{tokens.right}</span>
             </th>
             <th className="text-right pt10 pb10 pl5 pr5 font-weight-normal color-black-3 zb-b-b">
               <span hidden className="color-black-4 mr5 fs10">{tokens.left}</span>
@@ -51,8 +49,15 @@ const HelperOfMyOrders = ({orders={},dispatch})=>{
             </th>
             <th className="text-right pt10 pb10 pl5 pr5 font-weight-normal color-black-3 zb-b-b">Fee</th>
             <th className="text-right pt10 pb10 pl5 pr5 font-weight-normal color-black-3 zb-b-b">Filled</th>
-            <th className="text-center pl10 pr10 pt5 pb5 font-weight-normal color-black-3 zb-b-b">
-              <a className="fs12" onClick={cancelOrderByTokenPair.bind(this)}>Cancel All</a>
+            <th className="text-center pl10 pr10 pt5 pb5 font-weight-normal color-black-3 zb-b-b text-nowrap">
+              {
+                orders.items && orders.items.length > 0 &&
+                <a className="fs12" onClick={cancelOrderByTokenPair.bind(this)}>Cancel All</a>
+              }
+              {
+                orders.items && orders.items.length == 0 && 'Status'
+              }
+
             </th>
 
           </tr>
@@ -81,7 +86,7 @@ const HelperOfMyOrders = ({orders={},dispatch})=>{
             orders.items && orders.items.length == 0 &&
             <tr>
               <td className="zb-b-b pt10 pb10 pl5 pr5 text-center color-black-3 fs12" colSpan='100'>
-                no open {market} orders
+                no open orders of {market}
               </td>
             </tr>
           }
