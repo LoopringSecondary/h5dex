03d16895946a8c9462c2691fb8b72e813938960f
houzongbao <houzongbao@126.com>
2018-06-25 21:31:43 +0800

    fix bug for auth routes

--- src/auth/routes.js
+++ src/auth/routes.js
@@ -12,24 +12,25 @@ const routes = ()=>{
       case 'imtoken':
         return (
           <Switch>
-            <Route path={`/${url}/imtoken`} exact component={AuthByImtoken} />
-            <Redirect to={`/${url}/imtoken`} />
+            <Route path={`/auth`} exact component={AuthByImtoken} />
+            <Route path={`/auth/imtoken`} exact component={AuthByImtoken} />
+
           </Switch>
         )
         break;
       case 'loopr':
         return (
           <Switch>
-            <Route path={`/${url}/loopr`} exact component={AuthByLoopr} />
-            <Redirect to={`/${url}/loopr`} />
+            <Route path={`/auth`} exact component={AuthByLoopr} />
+            <Route path={`/auth/loopr`} exact component={AuthByLoopr} />
           </Switch>
         )
         break;
       default:
         return (
           <Switch>
-            <Route path={`/${url}/address`} exact component={AuthByAddress} />
-            <Redirect to={`/${url}/address`} />
+            <Route path={`/auth`} exact component={AuthByAddress} />
+            <Route path={`/auth/address`} exact component={AuthByAddress} />
           </Switch>
         )
       break;
