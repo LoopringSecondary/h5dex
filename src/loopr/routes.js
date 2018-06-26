030410856dce0a6d0ad02cfa0bc61a86e12b14cd
lyd <lyddream@126.com>
2018-06-26 10:46:35 +0800

    fix loopr

--- src/loopr/routes.js
+++ src/loopr/routes.js
@@ -2,25 +2,27 @@ import React from 'react';
 import Loopr from './loopr'
 import {Toast, Button} from 'antd-mobile'
 import routeActions from 'common/utils/routeActions'
+import {connect} from 'dva'
 
-export default class Routes extends React.Component{
+class Routes extends React.Component{
 
 
   componentDidMount () {
     Toast.loading('Loading configs...', 0, () => {
      Toast.success('Load complete !!!')
     })
+    const _this = this
       window.Wallet = new Loopr();
       window.Wallet.setConfigs().then(res => {
-        this.props.dispatch({type:'locales/setLocale', payload:{locale:window.Wallet.language}});
-        this.props.dispatch({type:'settings/preferenceChange',payload:{language:window.Wallet.language,currency:window.language.currency}})
-        this.props.dispatch({type: 'sockets/unlocked'});
+        _this.props.dispatch({type:'locales/setLocale', payload:{locale:window.Wallet.language}});
+        _this.props.dispatch({type:'settings/preferenceChange',payload:{language:window.Wallet.language,currency:window.Wallet.currency}})
+        _this.props.dispatch({type: 'sockets/unlocked'});
         Toast.hide()
       })
   }
 
   goToDex = () => {
-    routeActions.gotoPath('/test')
+    routeActions.gotoPath('/dex')
   }
 
   render () {
@@ -31,3 +33,5 @@ export default class Routes extends React.Component{
     )
   }
 }
+
+export default connect()(Routes)
