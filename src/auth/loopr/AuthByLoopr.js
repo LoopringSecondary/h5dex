9ece7f9b7acd6c93911545fb73a3ba9687e3933f
houzongbao <houzongbao@126.com>
2018-06-26 11:19:32 +0800

    update auth modules

--- src/auth/loopr/AuthByLoopr.js
+++ src/auth/loopr/AuthByLoopr.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import Loopr from './loopr'
-import {Toast, Button} from 'antd-mobile'
+import {Icon as WebIcon} from 'antd'
+import {Toast, Button,NavBar} from 'antd-mobile'
 import routeActions from 'common/utils/routeActions'
 
 export default class Routes extends React.Component{
-
-
   componentDidMount () {
     Toast.loading('Loading configs...', 0, () => {
      Toast.success('Load complete !!!')
@@ -18,15 +17,29 @@ export default class Routes extends React.Component{
         Toast.hide()
       })
   }
-
   goToDex = () => {
     routeActions.gotoPath('/test')
   }
-
   render () {
     return (
       <div>
-        <Button type="primary" onClick={this.goToDex}>进去DEX</Button>
+        <NavBar
+          className=""
+          mode="light"
+          leftContent={null &&[
+            <span onClick={()=>{}} className="color-black-1" key="1"><WebIcon type="left" /></span>,
+          ]}
+          rightContent={null && [
+            <span className="color-black-1" key="1"  onClick={()=>{}}><WebIcon type="question-circle-o" /></span>
+          ]}
+        >
+          <div>
+            Auth By Loopr
+          </div>
+        </NavBar>
+        <div className="p15">
+          <Button type="primary" onClick={this.goToDex}>进去DEX</Button>
+        </div>
       </div>
     )
   }
