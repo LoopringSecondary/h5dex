e2cc5d6867538824775df74713869d5d4aecc71c
hzbdesigner <hzbdesigner@gmail.com>
2018-09-26 14:06:01 +0800

    fix ui bug for setting

--- src/dex/account/Settings.js
+++ src/dex/account/Settings.js
@@ -97,14 +97,13 @@ function Settings(props) {
                 </RadioItem>
               ))}
             </List>
-            <List className="m10 no-border text-left" renderHeader={() => <div className="fs13 color-black-4 mb5 mt15 pl5">{intl.get('settings.trading_fee')}</div>}>
-              <List.Item className="">
-                <div className="pt10 pb10">
+            <List className="m10 no-border text-left position-relative" renderHeader={() => <div className="fs13 color-black-4 mb5 mt15 pl5">{intl.get('settings.trading_fee')}</div>}>
+              <List.Item className="pt10 pb10 overflow-visible" >
+                <div className="pt10 pb10 pl10">
                   <Slider
-                    style={{ }}
                     defaultValue={settings.trading.lrcFee}
                     min={1}
-                    max={50}
+                    max={20}
                     onChange={(v)=>lrcFeeChange(v)}
                     onAfterChange={()=>{}}
                   />
@@ -119,8 +118,8 @@ function Settings(props) {
               </List.Item>
             </List>
             {
-              true &&
-              <List className="m10 no-border text-left" renderHeader={() => <div className="fs13 color-black-4 mb5">Currency</div>}>
+              false &&
+              <List className="m10 no-border text-left" renderHeader={() => <div className="fs13 color-black-4 mb5">Order Time-To-Live</div>}>
                 {ttls.map(i => (
                   <RadioItem className="zb-b-b" key={i.value} checked={i.checked} onChange={() => languageChange(i.value)}>
                     {i.label}
