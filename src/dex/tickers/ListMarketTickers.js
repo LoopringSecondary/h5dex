8a03d9796db72c62f8803efbe4214da97f0e0a82
hzbdesigner <hzbdesigner@gmail.com>
2018-09-28 23:30:59 +0800

    update tickers

--- src/dex/tickers/ListMarketTickers.js
+++ src/dex/tickers/ListMarketTickers.js
@@ -176,7 +176,8 @@ export const TickerList = ({items,loading,dispatch, tickersList})=>{
   }
 
   return (
-    <div className="" style={{minHeight:'50vh'}}>
+    <div className="bg-white" style={{minHeight:'50vh'}}>
+      <div className="divider 1px zb-b-t"></div>
       <Spin spinning={loading}>
         {!loading && items.length > 0 &&
           <div>
